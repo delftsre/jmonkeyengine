5d8368d35f0f7df8f156c45c3350b9b23ffb5421
Enrique Correa <quiquecorreav@gmail.com>
2016-05-03 15:06:47 +0200

    added some testing to math functions

--- jme3-core/src/test/java/com/jme3/math/FastMathTest.java
+++ jme3-core/src/test/java/com/jme3/math/FastMathTest.java
@@ -32,6 +32,8 @@
 package com.jme3.math;
 
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Verifies that algorithms in {@link FastMath} are working correctly.
@@ -54,6 +56,59 @@ public class FastMathTest {
             assert i <= nextPowerOf2;
             assert FastMath.isPowerOfTwo(nextPowerOf2);
             assert nextPowerOf2 == nearestPowerOfTwoSlow(i);
+        }
+    }
+
+    @Test
+    public void testIsPowerOfTwo() {
+        assertFalse(FastMath.isPowerOfTwo(-16));
+        assertFalse(FastMath.isPowerOfTwo(-10));
+        assertFalse(FastMath.isPowerOfTwo(-8));
+        assertFalse(FastMath.isPowerOfTwo(-7));
+        assertFalse(FastMath.isPowerOfTwo(-4));
+        assertFalse(FastMath.isPowerOfTwo(-1));
+        assertFalse(FastMath.isPowerOfTwo(0));
+        assertTrue(FastMath.isPowerOfTwo(1));
+        assertTrue(FastMath.isPowerOfTwo(2));
+        assertFalse(FastMath.isPowerOfTwo(3));
+        assertTrue(FastMath.isPowerOfTwo(4));
+        assertFalse(FastMath.isPowerOfTwo(5));
+        assertFalse(FastMath.isPowerOfTwo(6));
+        assertFalse(FastMath.isPowerOfTwo(7));
+        assertTrue(FastMath.isPowerOfTwo(8));
+    }
+
+    @Test
+    public void interpolateLinear() {
+        for (float x = -3f; x < 3f; x++) {
+            for (float y = -3f; y < 3f; y++) {
+                assert x == FastMath.interpolateLinear(0f, x, y);
+                assert x == FastMath.interpolateLinear(-1.5f, x, y);
+                assert y == FastMath.interpolateLinear(1f, x, y);
+                assert y == FastMath.interpolateLinear(2.5f, x, y);
+                for (float scale = 0f; scale < 1f; scale += 0.2f) {
+                    float interpolation = (1-scale) * x + scale * y;
+                    assert interpolation == FastMath.interpolateLinear(scale, x, y); 
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testApproximateEquals() {
+        assertTrue(FastMath.approximateEquals(100000, 100001));
+        assertFalse(FastMath.approximateEquals(10000, 10001));
+    }
+
+    public float slowInvSqrt(float fValue) {
+        return (float) (1.0f / Math.sqrt(fValue));
+    }
+
+    @Test
+    public void testFastInvSqrt() {
+        for (float i = 0.1f; i < 5f; i+=0.1) {
+            // Is not very accurate
+            assertTrue(FastMath.abs(slowInvSqrt(i) - FastMath.fastInvSqrt(i)) < 0.1f);
         }
     }
 }
